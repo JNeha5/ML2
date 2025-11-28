cad65a9bcef1480ff251d8ccf4ac0ad750c95f35
Neha <nehajawahar5@gmail.com>
2025-11-28 13:05:02 +0530

    Final commit

---
+++ dataset_student.py
@@ -0,0 +1,24 @@
+\ (roll_no., name, subject, gpa, year)
+
+students_data = [
+    (101, 'Ram', 'CS', 3.5, 1),
+    (102, 'Bob', 'Bio', 3.2, 2),
+    (103, 'Shyam', 'Eng', 3.9, 4),
+    (104, 'Neha', 'Phys', 2.8, 3),
+    (105, 'soni', 'Arts', 3.1, 1),
+    (106, 'France', 'CS', 3.8, 2),
+    (107, 'Roni', 'Bio', 3.6, 4),
+    (108, 'Arunadha', 'Eng', 3.0, 3),
+    (109, 'Tarun', 'Phys', 3.4, 1),
+    (110, 'Judy', 'Arts', 3.7, 2),
+    (111, 'Mark', 'CS', 3.1, 4),
+    (112, 'Fisher', 'Bio', 3.9, 3),
+    (113, 'Sohana', 'Eng', 3.3, 1),
+    (114, 'Jeff', 'Phys', 3.5, 2),
+    (115, 'Ken', 'Arts', 3.2, 4),
+    (116, 'Arun', 'CS', 3.6, 3),
+    (117, 'Robin', 'Bio', 3.0, 1),
+    (118, 'Rgav', 'Eng', 3.7, 2),
+    (119, 'Jawahar', 'Phys', 3.8, 4),
+    (120, 'Saratha', 'Arts', 3.4, 3)
+]
